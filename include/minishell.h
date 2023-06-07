92a256a063e7aabf04c7db8a9d4de4d74129109f
Antonio Simone <asimone@f1r4s8.codam.nl>
2023-06-07 18:01:31 +0200

    Adding History

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,23 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 14:31:52 by asimone       #+#    #+#                 */
-/*   Updated: 2023/06/07 14:31:53 by asimone       ########   odam.nl         */
+/*   Updated: 2023/06/07 17:30:44 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef MINISHELL_H
+#define  MINISHELL_H
+
+#define GREEN "\x1b[32;01m"
+#define RED "\x1b[31;01m"
+#define YELLOW "\033[33;1m"
+#define BOLD "\033[1m"
+#define RESET  "\x1b[0m"
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,22 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 14:32:04 by asimone       #+#    #+#                 */
-/*   Updated: 2023/06/07 14:33:12 by asimone       ########   odam.nl         */
+/*   Updated: 2023/06/07 17:50:57 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../include/minishell.h"
 
 int main()
 {
-	printf("KURWA!\n");
-	return (0);
+	char *str;
+	
+	while (1)
+	{
+		str = readline( GREEN BOLD "mustash> "RESET);
+		add_history(str);
+		printf("line = %s\n", str);
+		free(str);
+	}
+    return 0;
 }
