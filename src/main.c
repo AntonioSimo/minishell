5a12b82482b74d474614f71351388fdac78253ed
Antonio Simone <asimone@f1r4s8.codam.nl>
2023-06-07 18:37:28 +0200

    Try rl_replace_line

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 14:32:04 by asimone       #+#    #+#                 */
-/*   Updated: 2023/06/07 18:13:09 by asimone       ########   odam.nl         */
+/*   Updated: 2023/06/07 18:33:03 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int main()
 		str = readline( GREEN BOLD "mustash> "RESET);
 		add_history(str);
 		//rl_replace_line("haha",0);
-		printf("line = %s\n", str);
+		//printf("line = %s\n", str);
+		rl_redisplay();
 		free(str);
 	}
     return 0;
