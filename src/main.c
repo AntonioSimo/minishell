6a2c127379f04deda39bf14e013c0065cde56f87
Patryk Skrucha <pskrucha@f1r4s20.codam.nl>
2023-06-14 16:40:02 +0200

    finding path and splitting into single dir paths

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: asimone <asimone@student.42.fr>              +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/06/07 14:32:04 by asimone       #+#    #+#                 */
-/*   Updated: 2023/06/13 20:39:01 by asimone       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 14:32:04 by asimone           #+#    #+#             */
+/*   Updated: 2023/06/14 15:27:08 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,27 +57,63 @@
 char *get_path(char **env)
 {
 	int i;
+	int j;
 	//printf("here \n");
 
 	i = 0;
+	j = 6;
 	while (env[i])
 	{
 		//printf("line: %s\n", env[i]);
 		if (!ft_strncmp("PATH=", env[i], 5))
+		{
+			while (--j)
+				env[i]++;
 			return (env[i]);
+		}
 		i++;
 	}
 	return ("NIC");
 	
 }
 
-void	loop()
+void	execute_command(char **single_path, char **env)
+{
+	char* arr[] = {"/ls", "-la", NULL};
+	int i;
+	char *path_with_command;
+
+	
+	//(void)env;
+	i = 0;
+	while (single_path[i])
+	{
+		path_with_command = ft_free_strjoin(single_path[i], arr[0]);
+		if (!path_with_command)
+			exit(EXIT_FAILURE);
+		printf("path with command: >>%s<<\n", path_with_command);
+		//printf("path: >>%s<<\n", single_path[i]);
+		execve(path_with_command, arr, env);
+		i++;
+		free(path_with_command);
+		path_with_command = NULL;
+	}
+	printf("command not found\n");
+	exit(EXIT_FAILURE);
+}
+
+void	loop(char **env)
 {
 	char *line;
 	char **args;
 	pid_t	pid;
 	int  status;
-	char pwd[100];
+	char pwd[FILENAME_MAX + 1];
+	char *path;
+	char **single_path;
+
+	path = get_path(env);
+	printf("path: %s", path);
 	
 	while (1)
 	{
@@ -87,12 +123,13 @@ void	loop()
 	//rl_on_new_line();
 		if (!ft_strncmp("ls", *args, 3))
 		{
+			single_path = ft_split(path, ':');
+			if (!single_path)
+				exit(EXIT_FAILURE);
 			pid = fork();
 			if (pid == 0)
 			{
-				char* arr[] = {"ls", "-la", NULL};
-				execv("/bin/ls", arr);
-				exit(EXIT_SUCCESS);
+				execute_command(single_path, env);
 			}
 			waitpid(pid, &status, 0);
 		}
@@ -100,32 +137,24 @@ void	loop()
 		{
 			printf("%s\n", getcwd(pwd, sizeof(pwd)));
 		}
-		if (!ft_strncmp("cd", args[0], 3) && !ft_strncmp("..", args[1], 3))
+		if (!ft_strncmp("cd", args[0], 3))
 		{
-			printf("im here!\n");
-			chdir("../");
+			if (chdir(args[1]))
+				printf("no such file or directory\n");
 		}
 		else if (!ft_strncmp("exit", *args, 5))
 			exit(EXIT_SUCCESS);
 	}
 	free(line);
 	free(args);
-}
+}	
 
-int	main(int argc, char** argv, char** envep)
+int	main(int argc, char** argv, char** env)
 {
 	//int i = 0;
 	(void) argv;
 	(void) argc;
 	//char pwd[100];
-
-	char *path = get_path(envep);
-
-	path++;
-	path++;
-	path++;
-	path++;
-	path++;
 
 	//printf("path: %s\n", getcwd(pwd, 100));
 	//printf("path: %s\n", getcwd(pwd, 100));
@@ -142,7 +171,7 @@ int	main(int argc, char** argv, char** envep)
 	while (1)
 	{
 		
-		loop();	
+		loop(env);	
 	}
 	exit (EXIT_SUCCESS);
 }
