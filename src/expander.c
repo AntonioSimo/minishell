225640223b5639a69e941b12636231bb0b1746a9
PatrykSkrucha <patryk.skrucha@gmail.com>
2023-08-29 15:14:32 +0200

    working on expander

--- src/expander.c
+++ src/expander.c
@@ -12,24 +12,37 @@
 
 #include "../include/minishell.h"
 
+char	*find_expandable(char *str)
+{
+	int i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '$')
+		i++;
+	}
+}
+
 static void	update_token(t_token *tokens, t_envepval *my_env)
 {
-	//char *str;
+	char *str;
 	(void) my_env;
 	(void) tokens;
 
-	printf("jest $!\n");
+	str = find_expandable(tokens->command);
+	printf("%s\n", tokens->command);
 }
 
 void	expander(t_token *tokens, t_envepval *my_env)
 {
 	while (tokens)
 	{
-		if (tokens->type == DEFAULT || tokens->type == DOUBLE_QUOTED)
-		{
-			if (ft_strchr(tokens->command, '$'))
+		if ((tokens->type == DEFAULT || tokens->type == DOUBLE_QUOTED)
+			&& ft_strchr(tokens->command, '$'))
 				update_token(tokens, my_env);
-		}
 		tokens = tokens->next;
 	}
 }
