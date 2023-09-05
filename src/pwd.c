cc1629656191213584d6ba44b94fd55da2ca2ef2
Antonio Simone <asimone@f1r4s2.codam.nl>
2023-09-05 12:23:25 +0200

    Adding pwd command

--- src/pwd.c
+++ src/pwd.c
@@ -1,1 +1,21 @@
 #include "../include/minishell.h"
+
+#include "../include/minishell.h"
+
+/**
+* @brief	This function replicate the behavior of pwd command
+* @param 	void This function doesn't need a param
+* @return 	It will return the current working directory saved in the variable temp.
+*/
+void	get_current_working_dir(void)
+{
+	char	pwd[FILENAME_MAX + 1];
+	char	*temp;
+
+	ft_memset(pwd, 0, sizeof(pwd)); //initialize pwd to zero to not print weird char
+	temp = malloc(sizeof(char *) * ft_strlen(pwd) + 1);
+	if (!temp)
+		free(temp);
+	temp = getcwd(pwd, sizeof(pwd));
+		ft_printf("%s\n", temp);
+}
