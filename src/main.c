8097eee28d46804c782e228df3c467a912e6093a
Antonio Simone <asimone@f1r4s2.codam.nl>
2023-09-05 11:55:24 +0200

    Editing main function

--- src/main.c
+++ src/main.c
@@ -6,14 +6,43 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 14:32:04 by asimone       #+#    #+#                 */
-/*   Updated: 2023/09/05 11:51:23 by asimone       ########   odam.nl         */
+/*   Updated: 2023/09/05 11:54:43 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int main()
+void	ft_free(char *str)
 {
-	printf("KURWA!\n");
-	return (0);
+	if (str)
+	{
+		free(str);
+		str = NULL;
+	}
+}
+
+void	loop(char **env, t_envepval *my_env)
+{
+	char *line;
+
+	(void) my_env;
+	(void)env;
+	while (1)
+	{
+		line = readline(GREEN BOLD "mustash> "RESET);
+		if (!line)
+			break ;
+		add_history(line);
+		ft_free(line);
+	}
+}	
+
+int	main(int argc, char** argv, char** env)
+{
+	(void) argv;
+	(void) argc;
+	t_envepval	*my_env = NULL;
+
+	loop(env, my_env);	
+	exit (EXIT_SUCCESS);
 }
