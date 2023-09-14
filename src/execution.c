4ad1e023b3ff2b8c6a3d71a1c97460ba40e449b9
PatrykSkrucha <patryk.skrucha@gmail.com>
2023-09-15 01:17:10 +0200

    building commands

--- src/execution.c
+++ src/execution.c
@@ -3,13 +3,13 @@
 void    redir_out(t_command *cmd, int *fd)
 {   
     int		fileout;
-	char	msg[1024];
+	char	*msg;
 
 	fileout = open(cmd->command, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     // dup2(fd[0], STDIN_FILENO);
 	dup2(fileout, STDOUT_FILENO);
-	read(fd[1], msg, 1024);
-	printf("%s", msg);
+	// printf("%s", msg);
+	read(fd[0], msg, O_NONBLOCK);
 	close(fd[1]);
 	// run_cat(env);
     exit(EXIT_SUCCESS);
@@ -30,6 +30,7 @@ void	test_cmd(t_command	*cmd, t_envepval *env)
 
 void	execute_pipe(t_command *cmd, t_envepval *env, int *fd)
 {	
+	printf("testy\n");
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
 	test_cmd(cmd, env);
@@ -37,7 +38,7 @@ void	execute_pipe(t_command *cmd, t_envepval *env, int *fd)
 
 void execute_second(t_command *cmd, t_envepval *env, int *fd)
 {
- printf("at before\n");   
+ //printf("at before\n");   
     dup2(fd[0], STDIN_FILENO);
     close(fd[1]);
     test_cmd(cmd, env);
@@ -58,7 +59,8 @@ void	run_commands(t_command *cmds, t_envepval *env)
 		if (pid == -1)
 			perror_exit("Fork error\n");
 		if (pid == 0)	
-            execute_pipe(cmds, env, fd);
+			execute_pipe(cmds, env, fd);
+            // test_cmd(cmds, env);
 		 waitpid(pid, NULL, 0);
 		cmds = cmds->next;
 		pid = fork();
@@ -68,6 +70,6 @@ void	run_commands(t_command *cmds, t_envepval *env)
 		if (pid == 0)	
 			redir_out(cmds,fd);
         //     execute_second(cmds->next, env, fd);
-		//waitpid(pid, NULL, 0);
+		waitpid(pid, NULL, 0);
 	// }
 }
