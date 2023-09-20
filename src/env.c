b283df8597e7b52e07fbab901de9cec847d0b582
Antonio Simone <asimone@f0r4s24.codam.nl>
2023-09-20 17:45:11 +0200

    Adding some env functions

--- src/env.c
+++ src/env.c
@@ -48,6 +48,7 @@ void	set_env(t_envepval	**my_env, char **env)
 	char *key;
 	char *value;
 	int equal_pos;
+	char    *shlvl;
 
 	i = 0;
 	while (env[i])
@@ -55,9 +56,57 @@ void	set_env(t_envepval	**my_env, char **env)
 		equal_pos = find_equal(env[i]);
 		key = ptr_check(ft_substr(env[i], 0, equal_pos));
 		value = ptr_check(ft_substr(env[i], equal_pos + 1, ft_strlen(env[i]) - equal_pos));
+		if (!ft_strncmp(key, "SHLVL", 5))
+        {
+               shlvl = ft_itoa(ft_atoi(value) + 1);
+               free(value);
+                value = ft_strdup(shlvl);
+                free(shlvl);
+        }
 		envlst_add(my_env, create_env_node(key, value));
 		ft_free(key);
 		ft_free(value);
 		i++;
 	}
 }
+
+size_t env_len(t_envepval *env)
+{
+    size_t i;
+    i = 0;
+    if (!env)
+            return (0);
+    while (env)
+    {
+            i++;
+            env = env->next;
+    }
+    return (i);
+}
+
+t_env  *copy_env(char **env)
+{
+       t_env   *new_env;
+       t_envepval       *env_lst;
+       size_t  size;
+       int i;
+       char    *key;
+
+       i = 0;
+	   env_lst = NULL;
+       set_env(&env_lst, env);
+       new_env = ptr_check(malloc(sizeof(t_env)));
+       new_env->env = env_lst;
+       size = env_len(env_lst);
+       new_env->env_copy = malloc(sizeof(char *) * (size + 1));
+       while(env_lst)
+       {
+               key = ptr_check(ft_strjoin(env_lst->key, "="));
+               new_env->env_copy[i] = ptr_check(ft_strjoin(key, env_lst->val));
+               free(key);
+               env_lst = env_lst->next;
+               i++;
+       }
+       new_env->env_copy[i] = NULL;
+       return (new_env);
+ }
