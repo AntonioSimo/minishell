e05197d9c099d4f5fefb799e53dc7c5e180f9b0e
PatrykSkrucha <patryk.skrucha@gmail.com>
2023-10-05 16:11:51 +0200

    norm fix

--- src/cd.c
+++ src/cd.c
@@ -1,78 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:09:40 by pskrucha          #+#    #+#             */
+/*   Updated: 2023/10/05 16:11:09 by pskrucha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
 
-char    *get_cwd()
+char	*get_cwd(void)
 {
-    char    *cwd;
+	char	*cwd;
 
-    cwd = malloc(PATH_MAXSIZE);
-    if (!cwd)
-        return (NULL);
-    if (getcwd(cwd, PATH_MAXSIZE))
-        return (cwd);
-    else 
-    {
-        perror("getcwd() error");
-        exit (EXIT_FAILURE);
-    }
+	cwd = malloc(PATH_MAXSIZE);
+	if (!cwd)
+		return (NULL);
+	if (getcwd(cwd, PATH_MAXSIZE))
+		return (cwd);
+	else
+	{
+		perror("getcwd() error");
+		exit (EXIT_FAILURE);
+	}
 }
 
-char    *get_pwd(t_env  *env)
+char	*get_pwd(t_env *env)
 {
-    t_envepval  *variable;
+	t_envepval	*variable;
 
-    variable = env->env;
-    while (variable != NULL)
-    {
-        if (ft_strcmp(variable->key, "OLDPWD") == 0)
-            return (variable->val);
-        variable = variable->next;
-    }
-    return (NULL);
+	variable = env->env;
+	while (variable != NULL)
+	{
+		if (ft_strcmp(variable->key, "OLDPWD") == 0)
+			return (variable->val);
+		variable = variable->next;
+	}
+	return (NULL);
 }
 
-void    update_pwd(t_env *env, char *pwd)
+void	update_pwd(t_env *env, char *pwd)
 {
-    t_envepval  *variable;
+	t_envepval	*variable;
 
-    variable = env->env;
-    while (variable != NULL)
-    {
-        if (ft_strcmp(variable->key, "OLDPWD") == 0)
-        {
-            free(variable->val);
-            variable->val = ft_strdup(pwd);
-            return ;
-        }
-        variable = variable->next;
-    }
+	variable = env->env;
+	while (variable != NULL)
+	{
+		if (ft_strcmp(variable->key, "OLDPWD") == 0)
+		{
+			free(variable->val);
+			variable->val = ft_strdup(pwd);
+			return ;
+		}
+		variable = variable->next;
+	}
 }
 
-void    ft_cd(t_env *env, char **args)
+void	ft_cd(t_env *env, char **args)
 {
-    char    *pwd;
-    char    *nwd;
+	char	*pwd;
+	char	*nwd;
 
-
-    pwd = get_cwd();
-    nwd = NULL;
-    if (args[1] == NULL)
-    {
-        nwd = find_expandable(env->env, "HOME");
-        if (ft_strlen(nwd) == 0)
-        {
-            free(nwd);
-            nwd = find_home(env->env);
-        }
-    }
-    else if(!ft_strcmp(args[1], "-"))
-        nwd = find_expandable(env->env, "OLDPWD");
-    else if(args[1])
-        nwd = ft_strdup(args[1]);
-    if (chdir(nwd) != 0)
-        perror("chdir() error");
-    else
-        update_pwd(env, pwd);
-    free(pwd);
-    if (nwd)
-        free(nwd);
+	pwd = get_cwd();
+	nwd = NULL;
+	if (args[1] == NULL)
+	{
+		nwd = find_expandable(env->env, "HOME");
+		if (ft_strlen(nwd) == 0)
+		{
+			free(nwd);
+			nwd = find_home(env->env);
+		}
+	}
+	else if (!ft_strcmp(args[1], "-"))
+		nwd = find_expandable(env->env, "OLDPWD");
+	else if (args[1])
+		nwd = ft_strdup(args[1]);
+	if (chdir(nwd) != 0)
+		perror("chdir() error");
+	else
+		update_pwd(env, pwd);
+	free(pwd);
+	if (nwd)
+		free(nwd);
 }
