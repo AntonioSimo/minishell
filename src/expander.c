59c8671aab611b61dfcd1213f7f9e138922cbe0c
patryk <patryk@ubuntu2.myguest.virtualbox.org>
2023-10-08 01:44:39 +0200

    norm fix

--- src/expander.c
+++ src/expander.c
@@ -12,123 +12,76 @@
 
 #include "../include/minishell.h"
 
-extern int g_error_code;
+extern int	g_error_code;
 
-t_token	*create_new_nodes(char *expanded)
+t_expander	*set_var(void)
 {
-	char	**temp_arr;
-	t_token	*new_nodes;
-	int		i;
-	char	*token;
+	t_expander	*var;
 
-	i = 0;
-	new_nodes = NULL;
-	if (ft_strlen(expanded) == 0)
-	{
-		push_token(&new_nodes, lst_token_new("", DEFAULT));
-		return (new_nodes);
-	}
-	temp_arr = ptr_check(ft_split(expanded, ' '));
-	while (temp_arr[i])
-	{
-		token = ptr_check(ft_strdup(temp_arr[i]));
-		push_token(&new_nodes, lst_token_new(token, DEFAULT));
-		if (temp_arr[i + 1])
-			push_token(&new_nodes, lst_token_new(" ", SEPERATOR));
-		free(token);
-		i++;
-	}
-	temp_arr = double_free(temp_arr);
-	return (new_nodes);
+	var = ptr_check(malloc(sizeof(t_expander)));
+	var->i = 0;
+	var->prev_type = SEPERATOR;
+	var->old_pos = 0;
+	var->move_ptr = true;
+	return (var);
 }
 
-char	*replace_string(char *expanded, char	*str, int start, int end)
+void	single_dollar(t_token **tokens, t_envepval *my_env, \
+						t_token *head, t_expander *var)
 {
-	char	*before;
-	char	*after;
-	char	*temp;
-	char	*new_line;
-
-	before = ft_substr(str, 0, start - 1);
-	after = ft_substr(str, end, ft_strlen(str) - end);
-	temp = ft_strjoin(before, expanded);
-	new_line = ft_strjoin(temp, after);
-	free(temp);
-	free(before);
-	free(after);
-	return (new_line);
+	if (!dollar_expansion(*tokens, my_env, &head, var->i))
+	{
+		*tokens = head;
+		var->old_pos = var->i;
+		while (var->i--)
+			*tokens = (*tokens)->next;
+		var->i = var->old_pos;
+	}
+	else
+	{
+		var->move_ptr = false;
+		*tokens = head;
+		var->old_pos = var->i;
+		var->i--;
+		while (var->i-- >= 0)
+			*tokens = (*tokens)->next;
+		var->i = var->old_pos;
+	}
 }
 
+void	handle_double_dollar(t_token **tokens, t_token *head, t_expander *var)
+{
+	double_dollar(*tokens, &head, var->i);
+	*tokens = head;
+	var->old_pos = var->i;
+	while (var->i--)
+		*tokens = (*tokens)->next;
+	var->i = var->old_pos;
+}
 
 void	expander(t_token **tokens, t_envepval *my_env)
 {
-	t_token	*head;
-	int		i;
-	int		old_pos;
-	t_type	prev_type;
-	bool	move_ptr;
+	t_expander	*var;
+	t_token		*head;
 
-	i = 0;
 	head = *tokens;
-	prev_type = SEPERATOR;
+	var = set_var();
 	while (*tokens)
 	{
-		move_ptr = true;
-		if (if_tilde(tokens, prev_type))
+		var->move_ptr = true;
+		if (!if_tilde(tokens, var->prev_type))
 			tilde_expansion((*tokens), my_env);
-		if (((*tokens)->type == DEFAULT || (*tokens)->type == DOUBLE_QUOTED)
-			&& ft_strnstr((*tokens)->command, "$?", \
-			ft_strlen((*tokens)->command)))
-		{
-			error_code_expansion(*tokens, &head, i);
-			*tokens = head;
-			old_pos = i;
-			while (i--)
-				*tokens = (*tokens)->next;
-			i = old_pos;
-		}
-		if (((*tokens)->type == DEFAULT || (*tokens)->type == DOUBLE_QUOTED)
-			&& ft_strnstr((*tokens)->command, "$$", \
-			ft_strlen((*tokens)->command)))
-		{
-			double_dollar(*tokens, &head, i);
-			*tokens = head;
-			old_pos = i;
-			while (i--)
-				*tokens = (*tokens)->next;
-			i = old_pos;
-		}
-		if (((*tokens)->type == DEFAULT || (*tokens)->type == DOUBLE_QUOTED)
-			&& ft_strchr((*tokens)->command, '$')
-			&& ft_strlen((*tokens)->command) != 1)
-		{
-			if (!dollar_expansion(*tokens, my_env, &head, i))
-			{
-				*tokens = head;
-				old_pos = i;
-				while (i--)
-					*tokens = (*tokens)->next;
-				i = old_pos;
-			}
-			else
-			{
-				move_ptr = false;
-				*tokens = head;
-				old_pos = i;
-				i--;
-				while (i-- >= 0)
-					*tokens = (*tokens)->next;
-				i = old_pos;
-			}
-		}
-		if (*tokens && (*tokens)->type == SEPERATOR)
-			prev_type = SEPERATOR;
-		else
-			prev_type = DEFAULT;
-		if (move_ptr)
+		if (!is_error_code(tokens))
+			handle_error_code(tokens, head, var);
+		if (!is_double_dollar(tokens))
+			handle_double_dollar(tokens, head, var);
+		if (!is_single_dollar(tokens))
+			single_dollar(tokens, my_env, head, var);
+		check_prev_token(tokens, var);
+		if (var->move_ptr)
 		{
 			*tokens = (*tokens)->next;
-			i++;
+			var->i++;
 		}
 	}
 	*tokens = head;
--- src/parsing.c
+++ src/parsing.c
@@ -18,97 +18,89 @@ typedef struct s_parsing
 	char	**args_arr;
 }	t_parsing;
 
+t_parsing	*set_parsing_var(void)
+{
+	t_parsing	*var;
+
+	var = ptr_check(malloc(sizeof(t_parsing)));
+	var->args_arr = NULL;
+	var->word = NULL;
+	return (var);
+}
 
 void	handle_redirections(t_redir **redir, t_token *tokens)
 {
-		t_type	redir_type;
-		char	*file;
+	t_type	redir_type;
+	char	*file;
 
-		if (!*redir)
+	if (!*redir)
+	{
+		(*redir) = ptr_check(malloc(sizeof(t_redir)));
+		(*redir)->lst = NULL;
+		(*redir)->stdin_cpy = dup(STDIN_FILENO);
+		(*redir)->stdout_cpy = dup(STDOUT_FILENO);
+	}
+	redir_type = tokens->type;
+	while (tokens)
+	{
+		if (tokens->type == DEFAULT || tokens->type == DOUBLE_QUOTED
+			|| tokens->type == SINGLE_QUOTED)
 		{
-			(*redir) = ptr_check(malloc(sizeof(t_redir)));
-			(*redir)->lst = NULL;
-			(*redir)->stdin_cpy = dup(STDIN_FILENO);
-			(*redir)->stdout_cpy = dup(STDOUT_FILENO);
+			file = ptr_check(ft_strdup(tokens->command));
+			push_redir(&(*redir)->lst, lst_redir_new(file, redir_type));
+			break ;
 		}
-		redir_type = tokens->type;
-		while (tokens)
-		{
-			if (tokens->type == DEFAULT || tokens->type == DOUBLE_QUOTED
-				|| tokens->type == SINGLE_QUOTED)
-				{
-					file = ptr_check(ft_strdup(tokens->command));
-					push_redir(&(*redir)->lst, lst_redir_new(file, redir_type));
-					break ;
-				}
-			*tokens = *(tokens)->next;
-		}
+		*tokens = *(tokens)->next;
+	}
 }
 
-// static void	handle_cmds(t_token *tokens, t_command **commands, t_redir **redir, t_parsing **var)
-// {
-// 	if (is_word(tokens->type))
-// 	{
-// 		(*var)->word = ft_free_strjoin((*var)->word, tokens->command);
-// 	}
-// 	else if (tokens->type == SEPERATOR)
-// 	{
-// 		(*var)->args_arr = push_str_2d((*var)->args_arr, (*var)->word);
-// 		(*var)->word = ft_free((*var)->word);
-// 	}
-// 	else if (tokens->type == PIPE)
-// 	{
-// 		(*var)->args_arr = push_str_2d((*var)->args_arr, (*var)->word);
-// 		push_cmd(commands, lst_cmd_new((*var)->args_arr, *redir));
-// 		*redir = NULL;
-// 		(*var)->word = ft_free((*var)->word);
-// 		(*var)->args_arr = NULL;
-// 	}
-// }
+static void	handle_cmds(t_token *tokens, t_command **commands, \
+						t_redir **redir, t_parsing **var)
+{
+	if (is_word(tokens->type))
+	{
+		(*var)->word = ft_free_strjoin((*var)->word, tokens->command);
+	}
+	else if (tokens->type == SEPERATOR)
+	{
+		(*var)->args_arr = push_str_2d((*var)->args_arr, (*var)->word);
+		(*var)->word = ft_free((*var)->word);
+	}
+	else if (tokens->type == PIPE)
+	{
+		(*var)->args_arr = push_str_2d((*var)->args_arr, (*var)->word);
+		push_cmd(commands, lst_cmd_new((*var)->args_arr, *redir));
+		*redir = NULL;
+		(*var)->word = ft_free((*var)->word);
+		(*var)->args_arr = NULL;
+	}
+}
 
 t_command	*merge_tokens(t_token	*tokens)
 {
- 	t_command	*commands;
-	char		*word;
-	char		**args_arr;
+	t_command	*commands;
 	t_redir		*redir;
+	t_parsing	*var;
 
+	var = set_parsing_var();
 	commands = NULL;
-	args_arr = NULL;
 	redir = NULL;
-	word = NULL;
 	while (tokens)
 	{
 		if (tokens->type == REDIR_INPUT || tokens->type == REDIR_OUTPUT
 			|| tokens->type == REDIR_OUTPUT_APPEND || tokens->type == HEREDOC)
 			handle_redirections(&redir, tokens);
-		else if (is_word(tokens->type))
-		{
-			word = ft_free_strjoin(word, tokens->command);
-		}
-		else if (tokens->type == SEPERATOR)
-		{
-			args_arr = push_str_2d(args_arr, word);
-			word = ft_free(word);
-		}
-		else if (tokens->type == PIPE)
-		{
-			args_arr = push_str_2d(args_arr, word);
-			push_cmd(&commands, lst_cmd_new(args_arr, redir));
-			redir = NULL;
-			word = ft_free(word);
-			args_arr = NULL;
-		}
-		
+		else
+			handle_cmds(tokens, &commands, &redir, &var);
 		tokens = tokens->next;
 	}
-	if (word || args_arr || redir)
+	if (var->word || var->args_arr || redir)
 	{
-		args_arr = push_str_2d(args_arr, word);
-		if (args_arr || redir)
-			push_cmd(&commands, lst_cmd_new(args_arr, redir));
-		word = ft_free(word);
+		var->args_arr = push_str_2d(var->args_arr, var->word);
+		if (var->args_arr || redir)
+			push_cmd(&commands, lst_cmd_new(var->args_arr, redir));
+		free(var->word);
 		redir = NULL;
 	}
 	return (commands);
- }
+}
